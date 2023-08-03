eb4bbe78da4371af3fb024f4b8c0a504a587406f
Cynthia Castillo <ca.castillo@gmail.com>
2023-08-02 22:04:10 -0600

    Apoyalo + Poniendo todo junto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import PersonCard from "./componentes/PersonCard";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName="Elena" lastName="De Troya" age={18} hairColor="Black" />
+
+      <PersonCard firstName="Juana" lastName="De Arco" age={20} hairColor="Brown" />
+
+      <PersonCard firstName="Pedro" lastName="Paramo" age={32} hairColor="Blonde" />
+
+      <PersonCard firstName="Pablo" lastName="Picasso" age={50} hairColor="White" />
     </div>
   );
 }
---
+++ src/componentes/PersonCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class PersonCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            edad: this.props.age
+        }
+    }
+
+    render() {
+        const {lastName, firstName, hairColor} = this.props;
+
+        return (
+            <div className="row justify-content-center">
+                <div className="col-3 card mb-3 p-3">
+                    <h2>{lastName}, {firstName}</h2>
+                    <p>Edad: {this.state.edad}</p>
+                    <p>Color de Cabello: {hairColor}</p>
+                    <button className="btn btn-info" onClick={this.cumple}>Botón Cumpleaños {firstName}</button>
+                </div>
+            </div>
+        )
+    }
+
+    cumple = () => {
+        let nuevaEdad = this.state.edad +1;
+        this.setState({edad: nuevaEdad});
+    }
+
+}
+
+export default PersonCard;
